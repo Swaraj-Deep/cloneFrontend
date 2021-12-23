7b2ad53f6fa20adc0bcdcaa3c9a356a2a74fcbbe
Swaraj Deep <deep.swaraj7@gmail.com>
2021-12-23 23:51:22 +0530

    Add State

--- components/Registration/UserRegistration/UserRegistration.js
+++ components/Registration/UserRegistration/UserRegistration.js
@@ -1,6 +1,6 @@
 import Button from "../../../shared-components/Button";
 import Input from "../../../shared-components/Input";
-import { useState } from "react";
+import {useState} from "react";
 import Label from "../../../shared-components/Label";
 import styles from "./UserRegistration.module.css";
 
@@ -9,11 +9,17 @@ export default function UserRegistration(props) {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [dob, setDob] = useState("");
-  const [gender, setGender] = useState("");
+  const [gender, setGender] = useState("Male");
   const [address, setAddress] = useState("");
   const [phone, setPhone] = useState("");
   const [password, setPassword] = useState("");
   const [matchPassword, setMatchPassword] = useState("");
+
+  console.log("re render");
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(gender);
+  }
 
   return (
     <div className={styles.formpage}>
@@ -21,120 +27,139 @@ export default function UserRegistration(props) {
         <div>
           <h2 className={styles.h2}>Register</h2>
         </div>
+        <form onSubmit={handleSubmit}>
+          <div style={{display: "flex"}}>
+            <div className={styles.formElement}>
+              <Label text={"First Name:"} htmlFor={"firstName"}/>
+              <Input
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                type={"text"}
+                id={'firstName'}
+                size={"md"}
+              />
+            </div>
 
-        <div style={{ display: "flex" }}>
+            <div className={styles.formElement}>
+              <Label text={"Last Name:"} htmlFor={"lastName"}/>
+              <Input
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                type={"text"}
+                size={"md"}
+                id={'lastName'}
+              />
+            </div>
+          </div>
+
           <div className={styles.formElement}>
-            <Label text={"First Name:"} htmlFor={""} />
+            <Label text={"Email:"} htmlFor={"email"}/>
             <Input
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              type={"text"}
-              size={"md"}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type={"email"}
+              size={"lg"}
+              id={"email"}
             />
           </div>
 
           <div className={styles.formElement}>
-            <Label text={"Last Name:"} htmlFor={""} />
+            <Label text={"D.O.B:"} htmlFor={"dob"}/>
             <Input
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              type={"text"}
-              size={"md"}
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
+              type={"date"}
+              size={"lg"}
+              id={"dob"}
             />
           </div>
-        </div>
+          <div style={{padding: "0.3em"}}>
+            <Label text={"Gender:"}/>
+            <Input
+              type="radio"
+              value={gender}
+              name={'gender'}
+              id={"male"}
+              onChange={(e) => setGender("Male")}
+            />{" "}
+            <Label text={"Male"} htmlFor={'male'}/>
+            <Input
+              type="radio"
+              value={gender}
+              name={"gender"}
+              id={"female"}
+              onChange={(e) => setGender("Female")}
+            />{" "}
+            <Label text={"Female"} htmlFor={"female"}/>
+            <Input
+              type="radio"
+              value={gender}
+              name={"gender"}
+              id={"other"}
+              onChange={(e) => setGender("Others")}
+            />{" "}
+            <Label text={"other"} htmlFor={"other"}/>
+          </div>
 
-        <div className={styles.formElement}>
-          <Label text={"Email:"} htmlFor={""} />
-          <Input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type={"email"}
-            size={"lg"}
-          />
-        </div>
+          <div className={styles.formElement}>
+            <Label text={"Address:"} htmlFor={"address"}/>
+            <Input
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              type={"text"}
+              size={"lg"}
+              id={"address"}
+            />
+          </div>
 
-        <div className={styles.formElement}>
-          <Label text={"D.O.B:"} htmlFor={""} />
-          <Input
-            value={dob}
-            onChange={(e) => setDob(e.target.value)}
-            type={"date"}
-            size={"lg"}
-          />
-        </div>
-        <div style={{ padding: "0.3em" }}>
-          <Label text={"Gender:"} htmlFor={""} />
-          <input
-            type="radio"
-            value="Male"
-            name="gender"
-            onChange={(e) => setGender(e.target.value)}
-          />{" "}
-          Male
-          <input
-            type="radio"
-            value="Female"
-            name="gender"
-            onChange={(e) => setGender(e.target.value)}
-          />{" "}
-          Female
-          <input
-            type="radio"
-            value="Other"
-            name="gender"
-            onChange={(e) => setGender(e.target.value)}
-          />{" "}
-          Other
-        </div>
+          <div className={styles.formElement}>
+            <Label text={"Phone:"} htmlFor={"phone"}/>
+            <Input
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              type={"text"}
+              size={"lg"}
+              id={"phone"}
+            />
+          </div>
 
-        <div className={styles.formElement}>
-          <Label text={"Address:"} htmlFor={""} />
-          <Input
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            type={"text"}
-            size={"lg"}
-          />
-        </div>
+          <div className={styles.formElement}>
+            <Label text={"Password:"} htmlFor={"password"}/>
+            <Input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type={"password"}
+              size={"lg"}
+              id={"password"}
+            />
+          </div>
 
-        <div className={styles.formElement}>
-          <Label text={"Phone:"} htmlFor={""} />
-          <Input
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            type={"text"}
-            size={"lg"}
-          />
-        </div>
+          <div className={styles.formElement}>
+            <Label text={"Confirm Password:"} htmlFor={"confirmPassword"}/>
+            <Input
+              value={matchPassword}
+              onChange={(e) => setMatchPassword(e.target.value)}
+              type={"password"}
+              size={"lg"}
+              id={"confirmPassword"}
+            />
+          </div>
+          <div className={styles.formElement}>
+            <Button
+              text={"Join"}
+              type={"submit"}
+              btnStyle={"primary"}
+              onClick={() => console.log(firstName + " " + lastName)}
+            />
+            <Button
+              text={"Cancel"}
+              type={"button"}
+              btnStyle={"secondaryOutline"}
+              onClick={() => console.log(firstName + " " + lastName, gender)}
+            />
+          </div>
+        </form>
 
-        <div className={styles.formElement}>
-          <Label text={"Password:"} htmlFor={""} />
-          <Input
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type={"password"}
-            size={"lg"}
-          />
-        </div>
-
-        <div className={styles.formElement}>
-          <Label text={"Confirm Password:"} htmlFor={""} />
-          <Input
-            value={matchPassword}
-            onChange={(e) => setMatchPassword(e.target.value)}
-            type={"password"}
-            size={"lg"}
-          />
-        </div>
-        <div className={styles.formElement}>
-          <Button
-            text={"Join"}
-            type={"button"}
-            btnStyle={"primaryOutline"}
-            onClick={() => console.log(firstName + " " + lastName)}
-          />
-        </div>
       </div>
     </div>
   );
--- shared-components/Input/Input.js
+++ shared-components/Input/Input.js
@@ -1,8 +1,12 @@
 import styles from './Input.module.css';
 
-export default function Input({size, type, value, onChange, onClick}) {
+export default function Input({size, type, value, onChange, onClick, id, name}) {
   return <>
     <input
-      className={`${styles.input} ${size === 'sm' ? styles.inputSmall : size === 'md' ? styles.inputMedium : size === 'lg' ? styles.inputLarge : styles.input}`} type={type} value={value} onChange={onChange} onClick={onClick}/>
+      className={`${styles.input} ${size === 'sm' ? styles.inputSmall : size === 'md' ? styles.inputMedium : size === 'lg' ? styles.inputLarge : styles.input}`}
+      type={type} value={value} onChange={onChange} onClick={onClick}
+      id={id}
+      name={name}
+    />
   </>
 }
