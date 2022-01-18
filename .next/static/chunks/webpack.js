9717750474b9e31f250b7ce10f6d2efbb38f55d2
FaisalAhmad786 <faisalahmad7868@gmail.com>
2022-01-19 02:09:53 +0530

    Added search bar in header

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "617f11248d7ec8c4"; }
+/******/ 		__webpack_require__.h = function() { return "c41c2965d1704eb6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,4 +1,9 @@
 import styles from "./Home.module.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faSearch, faHighlighter } from "@fortawesome/free-solid-svg-icons";
+
 export default function HomePage() {
   return (
     <header className={`${styles.header}`}>
@@ -10,6 +15,31 @@ export default function HomePage() {
           <span className={`${styles.heading_primary_main}`}>Travel</span>
           <span className={`${styles.heading_primary_sub}`}>At your ease</span>
         </h1>
+      </div>
+      <div className={`${styles.headerSearchBar}`}>
+        <div className={`${styles.headerSearchBarSection}`}>
+          <label className={`${styles.headerSearchBarLabel}`}>From</label>
+          <input
+            placeholder="Pickup city"
+            className={`${styles.headerSearchBarInput}`}
+            type="text"
+          />
+        </div>
+        <div className={`${styles.headerSearchBarSection}`}>
+          <label className={`${styles.headerSearchBarLabel}`}>To</label>
+          <input
+            placeholder="where to go?"
+            className={`${styles.headerSearchBarInput}`}
+            type="text"
+          />
+        </div>
+        <div className={`${styles.headerSearchBarSection}`}>
+          <label className={`${styles.headerSearchBarLabel}`}>Date</label>
+          <input className={`${styles.headerSearchBarInput}`} type="date" />
+        </div>
+        <button className={`${styles.headerSearchBarButton}`}>
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
       </div>
     </header>
   );
